ef187b8703517d1e5df828d64a43e720a3afe6f6
nekonardo <yifan.zhong@cs.tum.de>
2024-02-04 17:25:32 +0100

    Basic communication over ROS

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View, ScrollView, SafeAreaView} from 'react-native';
+import {useState} from "react";
+import ROSLIB from 'roslib'
+import LoginPage from "./component/master";
+import {Stack,useRouter} from 'expo-router'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [status, setStatus] = useState("Not connected")
+    const [linear, setLinear] = useState({x: 0, y: 0, z: 0})
+    const [angular, setAngular] = useState({x: 0, y: 0, z: 0})
+    const ros = new ROSLIB.Ros({encoding: 'ascii'})
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            {/*<SendMessage/>*/}
+            <LoginPage/>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
---
+++ component/master.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react'
+import ROSLIB from 'roslib'
+import {Button, Text, View} from 'react-native';
+
+function LoginPage() {
+    const [status, setStatus] = useState("Not connected")
+    const [message, setMessage] = useState("No message")
+    const ros = new ROSLIB.Ros({encoding: 'ascii'})
+
+
+    function connect() {
+        ros.connect("ws://ubuntu.local:9090")
+        ros.on('error', function (error) {
+            console.log(error)
+            setStatus(error)
+        })
+
+        // Find out exactly when we made a connection.
+        ros.on('connection', function () {
+            // console.log('Connected!')
+            setStatus("Connected!")
+        })
+
+        ros.on('close', function () {
+            // console.log('Connection closed')
+            setStatus("Connection closed")
+        })
+    }
+
+    let listener = new ROSLIB.Topic({
+        ros: ros,
+        name: '/chatter',
+        messageType: 'std_msgs/String'
+    });
+
+    listener.subscribe(function (message) {
+        // console.log('Received message on ' + listener.name + ': ' + message.data);
+        setMessage(message.data)
+        // listener.unsubscribe();
+    });
+
+
+    return (
+        <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>{status}</Text>
+
+            <Text>{message}</Text>
+
+            <Button title="Connect" onPress={connect}/>
+        </View>
+
+
+    )
+}
+
+export default LoginPage
